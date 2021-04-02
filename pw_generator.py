f73aa14fd693441693ba69c9c3dfc058fbd39ad8
jachman88 <81709822+jachman88@users.noreply.github.com>
2021-04-02 11:28:36 -0700

    Add files via upload
    Simple password generator

--- pw_generator.py
+++ pw_generator.py
@@ -1,29 +1,36 @@
-#Password Generator build using Python 2
+#This is a simple password generator
+
+#! /bin/python3
 
 import random
 import string
+import sys
 
-pw_length = int(raw_input("Length of password: "))
-run_again =""
-#method that generates the password pool
-def pool_gen (pw_length):
-   chars=string.ascii_lowercase + string.ascii_uppercase
-   pwPool = chars + string.digits + string.punctuation
-   return pwPool
+pw_length = int(input("Length of password: "))
+run_again= ""
 
-#method that selects characters from the pool and builds the password
-def pw_build ():
-    counter = 0
-    new_pw = ""
-    while counter < pw_length:
-        pw_pool = pool_gen(pw_length)
-        position = random.randrange(len(pw_pool))
-        value = pw_pool[position]
-        new_pw = new_pw + value.__str__()
-        counter +=1
-    print (new_pw)
-    return new_pw
+#Function/Method that creates the pool of characters for the password
 
-pw_build()
+def pool_gen(pw_length):
+	chars=string.ascii_lowercase + string.ascii_uppercase
+	pwPool = chars + string.digits + string.punctuation
+	return pwPool
 
-raw_input('\n\nPress any key to exit')
+#Function that selects the characters from the pool and builds the password
+def pw_build():
+	counter = 0
+	new_pw = ""
+	while counter < pw_length:
+		pw_pool = pool_gen(pw_length)
+		position = random.randrange(len(pw_pool))
+		value = pw_pool[position]
+		new_pw = new_pw + value.__str__()
+		counter +=1
+	print(new_pw)
+	return new_pw
+
+try:
+	pw_build()	
+
+except KeyboardInterrupt:
+	print("\nTerminating Password Generator")
